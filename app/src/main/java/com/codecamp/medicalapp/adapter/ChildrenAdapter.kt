964f952d031e4a8e0046341303a8a0fb6318393d
unknown <ranaete14@gmail.com>
2022-04-08 09:33:34 -0700

    clean code added

--- app/src/main/java/com/codecamp/medicalapp/adapter/ChildrenAdapter.kt
+++ app/src/main/java/com/codecamp/medicalapp/adapter/ChildrenAdapter.kt
@@ -12,25 +12,25 @@ import com.codecamp.medicalapp.R
 import com.codecamp.medicalapp.model.Children
 import com.codecamp.medicalapp.viewholder.ChildrenViewHolder
 
-class ChildrenAdapter(var mChildrenList : ArrayList<Children>,var mContext : Context) : RecyclerView.Adapter<ChildrenViewHolder>()
+class ChildrenAdapter(var mChildrenList : ArrayList<Children>,var mContext : Context) : RecyclerView.Adapter<ChildrenViewHolder>(),ChildrenViewHolder.onItemClick
 {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ChildrenViewHolder {
-        var view = LayoutInflater.from(mContext).inflate(R.layout.children,parent,false)
+        val view = LayoutInflater.from(mContext).inflate(R.layout.children,parent,false)
         return ChildrenViewHolder(view);
     }
 
     override fun onBindViewHolder(holder: ChildrenViewHolder, position: Int) {
-        holder.mUserDesignation.setText(mChildrenList[position].designation);
-        mChildrenList[position].image?.let { holder.mUserImage.setImageResource(it) }
-        holder.mRelativeLayout.setOnClickListener {
-            holder.mRelativeLayout.startAnimation(AnimationUtils.loadAnimation(mContext,android.R.anim.fade_in))
-            val intent = Intent(mContext, DetailsActivity::class.java);
-            mContext.startActivity(intent);
-            (mContext as MainActivity).finish()
-        }
+        holder.bind(mChildrenList[position])
+        holder.onClick(holder.mRelativeLayout,mContext,::onNavigateToDetailsPage,position,holder)
     }
 
     override fun getItemCount(): Int {
         return mChildrenList.size
+    }
+
+    override fun onNavigateToDetailsPage(position: Int, holder: ChildrenViewHolder) {
+        val intent = Intent(mContext,DetailsActivity::class.java)
+        mContext.startActivity(intent)
+        (mContext as MainActivity).finish()
     }
 }
--- app/src/main/java/com/codecamp/medicalapp/viewholder/ChildrenViewHolder.kt
+++ app/src/main/java/com/codecamp/medicalapp/viewholder/ChildrenViewHolder.kt
@@ -1,10 +1,13 @@
 package com.codecamp.medicalapp.viewholder
 
+import android.content.Context
 import android.view.View
 import android.widget.RelativeLayout
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.codecamp.medicalapp.R
+import com.codecamp.medicalapp.model.Children
+import com.codecamp.medicalapp.util.fadeInAnimation
 import com.google.android.material.imageview.ShapeableImageView
 import com.google.android.material.shape.CornerFamily
 
@@ -20,6 +23,21 @@ class ChildrenViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView) {
         mUserImage.shapeAppearanceModel = roundedImage
         mUserDesignation = itemView.findViewById(R.id.designation);
         mRelativeLayout = itemView.findViewById(R.id.relativeLayout);
+    }
 
+    fun bind(children: Children){
+        mUserImage.setImageResource(children.image)
+        mUserDesignation.text = children.designation
+    }
+
+    fun onClick(widget : View, context: Context, onDoSomething : (position : Int, holder : ChildrenViewHolder) -> Unit, position: Int, holder: ChildrenViewHolder){
+        widget.setOnClickListener {
+            context.fadeInAnimation(widget)
+            onDoSomething(position,holder)
+        }
+    }
+
+    interface onItemClick{
+        fun onNavigateToDetailsPage(position: Int, holder: ChildrenViewHolder)
     }
 }
