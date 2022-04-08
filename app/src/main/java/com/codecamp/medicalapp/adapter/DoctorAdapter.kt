c035b1b2630a842f895d9c11bdc661cab35c4fc6
unknown <ranaete14@gmail.com>
2022-04-08 05:00:27 -0700

    clean code added

--- app/src/main/java/com/codecamp/medicalapp/adapter/DoctorAdapter.kt
+++ app/src/main/java/com/codecamp/medicalapp/adapter/DoctorAdapter.kt
@@ -15,20 +15,17 @@ import com.codecamp.medicalapp.viewholder.DoctorViewHolder
 
 class DoctorAdapter(var doctorList : ArrayList<Doctor>,var mContext : Context) : RecyclerView.Adapter<DoctorViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DoctorViewHolder {
-        var view = LayoutInflater.from(mContext).inflate(R.layout.doctors,parent,false);
+        val view = LayoutInflater.from(mContext).inflate(R.layout.doctors,parent,false);
         return DoctorViewHolder(view);
     }
 
     override fun onBindViewHolder(holder: DoctorViewHolder, position: Int) {
-        holder.dortorName.setText(doctorList[position].doctorName)
-        doctorList[position].doctorImage?.let { holder.doctorImage.setImageResource(it) }
-        holder.doctorDesignation.setText(doctorList[position].doctorDesignation)
-        holder.callButton.setOnClickListener {
-            holder.callButton.startAnimation(AnimationUtils.loadAnimation(mContext,android.R.anim.fade_in))
-            val intent = Intent(mContext,ChatActivity::class.java);
-            mContext.startActivity(intent);
-            (mContext as MainActivity).finish()
-        }
+        holder.bind(doctorList[position])
+        holder.onClick(holder.doctorConstraintLayout,mContext)
+        holder.onClick(holder.callButton,mContext)
+//        val intent = Intent(mContext,ChatActivity::class.java);
+//        mContext.startActivity(intent);
+//        (mContext as MainActivity).finish()
     }
 
     override fun getItemCount(): Int {
--- app/src/main/java/com/codecamp/medicalapp/viewholder/DoctorViewHolder.kt
+++ app/src/main/java/com/codecamp/medicalapp/viewholder/DoctorViewHolder.kt
@@ -1,10 +1,13 @@
 package com.codecamp.medicalapp.viewholder
 
+import android.content.Context
 import android.view.View
 import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
 import com.codecamp.medicalapp.R
+import com.codecamp.medicalapp.model.Doctor
+import com.codecamp.medicalapp.util.fadeInAnimation
 import com.google.android.material.imageview.ShapeableImageView
 import com.google.android.material.shape.CornerFamily
 
@@ -13,6 +16,7 @@ class DoctorViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView) {
     var doctorImage : ShapeableImageView;
     var doctorDesignation : TextView;
     var callButton : ConstraintLayout;
+    var doctorConstraintLayout : ConstraintLayout
     init {
         dortorName = itemView.findViewById(R.id.doctorName);
         doctorImage = itemView.findViewById(R.id.doctorImage);
@@ -22,5 +26,18 @@ class DoctorViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView) {
         doctorImage.shapeAppearanceModel = roundedImage
         doctorDesignation = itemView.findViewById(R.id.doctorDesignation);
         callButton = itemView.findViewById(R.id.callButton);
+        doctorConstraintLayout = itemView.findViewById(R.id.doctorConstraintLayout)
+    }
+
+    fun bind(doctor : Doctor){
+        dortorName.text = doctor.doctorName
+        doctor.doctorImage?.let { doctorImage.setImageResource(it) }
+        doctorDesignation.text = doctor.doctorDesignation
+    }
+
+    fun onClick(widget : View, context: Context){
+        widget.setOnClickListener {
+            context.fadeInAnimation(widget)
+        }
     }
 }
