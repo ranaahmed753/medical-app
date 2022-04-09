736d6810ac0603e4eef7911a379ce79c985be925
unknown <ranaete14@gmail.com>
2022-04-09 09:05:58 -0700

    clean code

--- app/src/main/java/com/codecamp/medicalapp/ChatActivity.kt
+++ app/src/main/java/com/codecamp/medicalapp/ChatActivity.kt
@@ -6,22 +6,32 @@ import android.os.Bundle
 import android.view.animation.AnimationUtils
 import android.widget.ImageView
 import androidx.constraintlayout.widget.ConstraintLayout
+import com.codecamp.medicalapp.util.fadeInAnimation
+import com.codecamp.medicalapp.util.navigate
 import com.codecamp.medicalapp.util.slideLeftAnimation
+import de.hdodenhof.circleimageview.CircleImageView
 
 class ChatActivity : AppCompatActivity() {
     private lateinit var mBackButton : ImageView;
     private lateinit var mConstraintLayout : ConstraintLayout
+    private lateinit var doctorImage : CircleImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat)
         mConstraintLayout = findViewById(R.id.constraintlayout)
+        doctorImage = findViewById(R.id.doctorImage)
 
         mBackButton = findViewById(R.id.backButton);
+        
         mBackButton.setOnClickListener {
-            mBackButton.startAnimation(AnimationUtils.loadAnimation(this,android.R.anim.fade_in));
-            startActivity(Intent(this,MainActivity::class.java));
-            finish();
+            fadeInAnimation(mBackButton)
+            navigate(this,MainActivity(),::finish)
+        }
+
+        doctorImage.setOnClickListener {
+            fadeInAnimation(doctorImage)
+            navigate(this,DetailsActivity(),::finish)
         }
     }
 
