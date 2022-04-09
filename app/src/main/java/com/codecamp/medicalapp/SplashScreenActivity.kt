7cdefcb2dc79cf6782dacfd1fbc59b1da4027ba6
unknown <ranaete14@gmail.com>
2022-04-09 08:58:32 -0700

    clean code

--- app/src/main/java/com/codecamp/medicalapp/SplashScreenActivity.kt
+++ app/src/main/java/com/codecamp/medicalapp/SplashScreenActivity.kt
@@ -4,11 +4,15 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.view.WindowManager
 import com.codecamp.medicalapp.util.navigate
 
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        getWindow().setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash_screen)
         Handler().postDelayed({
             navigate(this,MainActivity(),::finish)
